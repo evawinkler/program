2fb6e766e19884c4053d4a95df054b058baef5e7
Eva Winkler <winkler.eva00@gmail.com>
2020-08-09 11:54:08 +0200

    zacetek modela

--- model.py
+++ model.py
@@ -9,4 +9,12 @@ def osnovna_temperatura(stevilo, enota):
     else: 
         return False 
 
-osnovna_temperatura(40, kelvin)
+def izkana_temperatura( stevilo, enota):
+    if enota == 'kelvin':
+        return stevilo
+    elif enota == 'fahrenheit':
+        return (  stevilo * 9/ 5 ) - 459.67
+    elif enota == 'celzij': 
+        return stevilo - 273.15 
+    else: 
+        return False 
