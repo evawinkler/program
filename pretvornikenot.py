e22def9b14fd8afceeba7ea26963e4bb0e3fdf80
Eva Winkler <winkler.eva00@gmail.com>
2020-08-12 18:51:23 +0200

    spremenila v isto

--- pretvornikenot.py
+++ pretvornikenot.py
@@ -10,7 +10,7 @@ resitev = 0
 
 @bottle.get('/')
 def zacetna_stran():
-    return bottle.template('zacetna_stran.tpl', resitev=resitev)
+    return bottle.template('zacetna_stran.tpl', resitev = resitev)
 
 
 @bottle.post('/pretvori/')
